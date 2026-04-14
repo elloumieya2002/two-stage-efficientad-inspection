579bf247114d44a8cb410063f782373e02ed4054
elloumieya2002 <elloumieya28@gmail.com>
2026-04-14 11:03:58 +0100

    Update retrain_efficientad.py

--- scripts/retrain_efficientad.py
+++ scripts/retrain_efficientad.py
@@ -58,8 +58,8 @@ _HERE = os.path.dirname(os.path.abspath(__file__))
 if _HERE not in sys.path:
     sys.path.insert(0, _HERE)
 
-from models      import Teacher, Student, AutoEncoder
-from data_loader import get_AD_dataset, load_infinite, ImageNetDataset
+from utils.models      import Teacher, Student, AutoEncoder
+from utils.data_loader import get_AD_dataset, load_infinite, ImageNetDataset
 
 IMAGE_SIZE   = 256
 OUT_CHANNELS = 384
